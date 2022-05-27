25f8675a4237da782515838817b03d97938dc0a4
Bruna Souza <71838731+Bruunaa@users.noreply.github.com>
2022-05-26 22:52:07 -0300

    Aplicação Angular CRUD.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
+import { AuthModule } from './auth/auth.module';
+import { HotToastModule } from '@ngneat/hot-toast';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+    BrowserAnimationsModule,
+    CoreModule,
+    AuthModule, // As rotas em auth agora fazem parte do app
+    HotToastModule.forRoot({
+      position: 'bottom-center',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent], // primeiro componente a ser exibido
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'diario-viagens-272c6',
+    appId: '1:264893836085:web:8d190bd36da508fdc07a7c',
+    storageBucket: 'diario-viagens-272c6.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBdr4g226RTiZ04CvJ3odKFw7flrlWwNjw',
+    authDomain: 'diario-viagens-272c6.firebaseapp.com',
+    messagingSenderId: '264893836085',
+  },
   production: true
 };
