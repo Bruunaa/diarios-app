109477629be586c78e1b409cb3aa64ef343bc406
Bruna Souza <71838731+Bruunaa@users.noreply.github.com>
2022-06-02 23:22:25 -0300

    Atualizações e deploy

---
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { canActivate } from '@angular/fire/auth-guard';
+import { RouterModule, Routes } from '@angular/router';
+import { OnlyAdminGuard } from '../shared/guards/only-admin.guard';
+
+import { DashboardComponent } from './dashboard.component';
+
+const routes: Routes = [
+  {path:'dashboard', 
+  component: DashboardComponent,
+  canActivate: [OnlyAdminGuard],
+},
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DashboardRoutingModule { }
